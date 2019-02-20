b2f5206c46d7baeceb9ede30d0670eaac6590f35
Kgor <kevin.gorham@gmail.com>
2019-02-19 22:06:07 -0800

    started game.js, updated index.html

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+// Computer picks a letter at random
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+
+var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)]
+
+
+console.log(computerChoice);
+
+// Track wins, losses, guesses, guesses left
+
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+
+// If the user guesses correct they win the game and
+// the win counter increase by 1 
+document.onkeypress = function(event) {
+    var userGuess = event.key;
+
+
+    if (userGuess === computerChoice) {
+        MessageChannel.textContent = "You win!";
+        wins++;
+    } else {
+        guesses--;
+    }
+}
+
+document.getElementById('wins').innerHTML = "Wins: " + wins;
+document.getElementById('losses').innerHTML = "losses: " + losses;
+document.getElementById('guesses').innerHTML = "Guesses left: " + guesses;
